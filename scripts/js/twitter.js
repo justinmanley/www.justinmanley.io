19f4b1b66254879e5081cbf357aceb9d1896e9b9
Justin Manley <manleyjster@gmail.com>
2014-08-26 00:07:23 -0500

    Modify twitter.js script to get the oEmbed data for the retrieved tweets.

--- scripts/js/twitter.js
+++ scripts/js/twitter.js
@@ -2,12 +2,17 @@
 
 var Twitter = require('ntwitter'),
 	Q 		= require('q'),
+	_ 		= require('lodash'),
 	util 	= require("./util");
 
 var params = {
 	screen_name: 'outoftheyards',
-	count: 10
+	count: 20
 };
+
+function generateFeed(tweets) {
+	console.log(tweets);
+}
 
 util.readYAML('data/config/pass/twitter.yml')
 	.then(function(config) {
@@ -19,7 +24,14 @@ util.readYAML('data/config/pass/twitter.yml')
 				return Q.ninvoke(twitter, 'getUserTimeline', params);				
 			})
 			.then(function(timeline) {
-				console.log(timeline);
-			});
+				return Q.all(_.map(timeline, function(tweet) {
+					var oembedParams = { id: tweet.id_str };
+					return Q.ninvoke(twitter, 'getOEmbedStatus', oembedParams);
+				}));
+			})
+			.then(function(embeddedTweets) {
+				generateFeed(embeddedTweets);
+			})
+			.done();
 	})
 	.done();
