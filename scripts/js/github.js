119b2686530fc342ce4b3e5ba7cc99442df0bee1
Justin Manley <manleyjster@gmail.com>
2014-08-29 01:07:47 -0400

    Wrap each GitHub event in a div with class="event github-event".

--- scripts/js/github.js
+++ scripts/js/github.js
@@ -1,16 +1,22 @@
 var Q 			= require('q'),
 	_ 			= require('lodash'),
 	util 		= require('./util'),
-	moment 		= require('moment');
+	moment 		= require('moment'),
+	xmlbuilder 	= require('xmlbuilder');
 
 function generateFeed(items) {
+	var root = xmlbuilder.create('root');
+
 	return _.map(items, function(item) {
-		var updated = item.updated[0],
+		var event = root.ele('div', { class: "event github-event"}),
+			updated = item.updated[0],
 			timestamp = moment(updated).format(util.TIME_FORMAT);
+
+		event.raw(item.content[0]._);
 
 		return {
 			timestamp: moment(updated).format(),
-			html: item.content[0]._
+			html: event.toString({ pretty: true })
 		};
 	});
 }
