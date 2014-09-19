ffc27716526eb4302d65fe150c48b18ded805c48
Justin Manley <manleyjster@gmail.com>
2014-09-19 01:42:35 -0500

    Work on deployment script.

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,15 +1,25 @@
 git clone https://github.com/manleyjster/www.justinmanley.io.git
 
-npm install
-bower install
-
 cd www.justinmanley.io
 git checkout master
+
+npm install
+bower install
 
 # Retrieve and generate dynamic content and build site
 grunt build
 
-# deploy to gh-pages branch.  See: http://www.damian.oquanta.info/posts/one-line-deployment-of-your-site-to-gh-pages.html
+# allow the site's static assets to be recognized by git
+sed -i '/site\/assets\/bower_components/d' .gitignore
+
+# deploy to gh-pages branch.  See: 
+#	http://www.damian.oquanta.info/posts/one-line-deployment-of-your-site-to-gh-pages.html
+#   http://lukecod.es/2014/08/15/deploy-a-static-subdirectory-to-github-pages/
+git add --all site
+git commit -m "Generate website on $(date +\"%m-%d-%Y\") at $(date +\"%H-%M-%S\")."
+
+# check out a new local branch containing the site/ directory
 git subtree split --prefix site -b gh-pages
+
+# force push the gh-pages branch
 git push -f origin gh-pages:gh-pages
-git branch -D gh-pages
---
+++ scripts/deploy/secrets.sh
@@ -0,0 +1,1 @@
+# download secrets from AWS and place them in the appropriate places.
