d4496928edfab995dbfec6fef3eb6d25c881b94b
Justin Manley <manleyjster@gmail.com>
2014-09-19 01:40:01 -0500

    Refactor in order for feeds.js to be called from Grunt, rather than from the command line.

--- scripts/feeds/feeds.js
+++ scripts/feeds/feeds.js
@@ -1,46 +1,48 @@
 #!/usr/bin/node
 
-var Q 			= require('q'),
-	_ 			= require('lodash'),
-	util 		= require('./util'),
-	interleave 	= require('./interleave');
+var Q 				= require('q'),
+	_ 				= require('lodash'),
+	util 			= require('./util'),
+	configure		= require('./config'),
+	interleave 		= require('./interleave');
 
 Q.longStackSupport = true;
 
-util.readYAML('config/feeds.yml')
-	.then(processConfig)
-	.then(function(config) {
-		/* Require the corresponding transformer for each feed in the configuration. */
-		var feeds = _.map(_.keys(config.src), function(src) {
-			return { name: src, generate: require('./generators/' + src) }; 
-		});
+var secretFile = 'config/secret.yml',
+	configFile = 'config/feeds.yml';
 
-		/* Retrieve raw content and generate arrays of HTML snippets. */
-		return Q.all(_.map(feeds, function(feed) {
-			return feed.generate(config.src[feed.name]); 
-		}))
-		.then(function(feeds) { return _.flatten(feeds); })
-		.then(function(feed) {
-			this._config = config.src;
+module.exports = function(callback) {
+	configure(secretFile, configFile)
+		.then(function(config) {
+			/* Require the corresponding transformer for each feed in the configuration. */
+			var feeds = _.map(_.keys(config.src), function(src) {
+				return { name: src, generate: require('./generators/' + src) }; 
+			});
 
-			/* Serialize arrays of HTML snippets into HTML strings and write to files. */
-			var writers = _.map(config.dest, function(destination, type) {
-				util.writeFile(
-					destination, 
-					require('./htmlbuilders/' + type).serializeFeed.call(this, feed)
-				);
-			}, this);
-			return Q.all(writers);
+			/* Retrieve raw content and generate arrays of HTML snippets. */
+			return Q.all(_.map(feeds, function(feed) {
+				console.log('Extracting content from ' + feed.name + '...');
+				return feed.generate(config.src[feed.name]); 
+			}))
+			.then(function(feeds) { return _.flatten(feeds); })
+			.then(function(feed) {
+				this._config = config.src;
+
+				/* Serialize arrays of HTML snippets into HTML strings and write to files. */
+				var writers = _.map(config.dest, function(destination, type) {
+					console.log('Writing ' + destination + '...');
+					util.writeFile(
+						destination, 
+						require('./htmlbuilders/' + type).serializeFeed.call(this, feed)
+					);
+				}, this);
+				return Q.all(writers);
+			})
+			.then(function(result) {
+				if (callback) { callback(result); }
+				return result;
+			}) 
+			.done();
 		})
 		.done();
-	})
-	.done();
-
-/* Process configuration file. */
-function processConfig(config) {
-	_.each(config.src, function(srcConfig, srcName) {
-		srcConfig.name = srcName;
-	});
-
-	return config;
 }
