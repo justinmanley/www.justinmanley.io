17bbbf5d4a4a913dac1a9dc6bc559e0de35f6300
Justin Manley <manleyjster@gmail.com>
2014-09-20 15:21:32 -0500

    Change secrets.yml to secret.yml because of conflict with assemble YAML data processing.

--- scripts/deploy/download.sh
+++ scripts/deploy/download.sh
@@ -5,5 +5,5 @@ git clone https://github.com/manleyjster/www.justinmanley.io.git
 
 cd www.justinmanley.io
 
-scripts/deploy/secrets.sh
+scripts/deploy/secret.sh
 scripts/deploy/deploy.sh
--- scripts/deploy/secret.sh
+++ scripts/deploy/secret.sh
@@ -7,4 +7,4 @@ set -e
 aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.netrc /home/ec2-user/.netrc
 
 # get application secrets
-aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secrets.yml /home/ec2-user/www.justinmanley.io/config/secrets.yml
+aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml /home/ec2-user/www.justinmanley.io/config/secret.yml
