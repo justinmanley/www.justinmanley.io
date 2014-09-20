0bf741e8ecf07163749990c0e08da6772d4b7721
Justin Manley <manleyjster@gmail.com>
2014-09-20 17:04:54 -0500

    Change destination for secret.yml config file.

--- scripts/deploy/secret.sh
+++ scripts/deploy/secret.sh
@@ -5,33 +5,8 @@
 # end with nonzero exit code if any command fails 
 set -e
 
-echo '$ ls -al config/'
-ls -al config/
-
 # get application secrets
-echo '$ aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml ~/www.justinmanley.io/config/secret.yml'
-aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml ~/www.justinmanley.io/config/secret.yml
-
-echo '$ ls -al config/'
-ls -al config/
-
-echo '$ aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml secret.yml'
-aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml secret.yml
-
-echo '$ ls -al .'
-ls -al .
-
-echo '$ aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml config/secret.yml'
 aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml config/secret.yml
-
-echo '$ ls -al config/'
-ls -al config/
 
 # copy .netrc file to enable deploy.sh to commit to github
 aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.netrc ~/.netrc
-
-echo '$ echo $USER'
-echo $USER
-
-echo '$ ls -al ~ | grep netrc'
-ls -al ~ | grep netrc
