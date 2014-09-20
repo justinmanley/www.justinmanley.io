eaf27e8d90f144cea2e24af27247d73342f2c93b
Justin Manley <manleyjster@gmail.com>
2014-09-20 02:17:43 -0500

    Add line to secrets.sh to fetch application secrets from s3.

--- scripts/deploy/secrets.sh
+++ scripts/deploy/secrets.sh
@@ -1,4 +1,7 @@
 # download secrets from AWS and place them in the appropriate places.
 
 # copy .netrc file to enable deploy.sh to commit to github
-aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/github.netrc ~/.netrc
+aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.netrc ~/.netrc
+
+# get application secrets
+aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml ~/www.justinmanley.io/config/secret.yml
