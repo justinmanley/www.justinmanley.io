9e6dd9850383dd5e3825e4cab0b1641ffcb38eaa
Justin Manley <manleyjster@gmail.com>
2014-09-20 16:56:49 -0500

    Add debugging statements to secret.sh.

--- scripts/deploy/secret.sh
+++ scripts/deploy/secret.sh
@@ -12,17 +12,26 @@ ls -al config/
 echo '$ aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml ~/www.justinmanley.io/config/secret.yml'
 aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml ~/www.justinmanley.io/config/secret.yml
 
+echo '$ ls -al config/'
+ls -al config/
+
 echo '$ aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml secret.yml'
 aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml secret.yml
+
 echo '$ ls -al .'
 ls -al .
+
+echo '$ aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml config/secret.yml'
+aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.secret.yml config/secret.yml
+
+echo '$ ls -al config/'
+ls -al config/
 
 # copy .netrc file to enable deploy.sh to commit to github
 aws s3 cp s3://ZjY2ZGYyZGNlMDg5ZmY4NmU0YWYwNzgz/.secrets/www.justinmanley.io.netrc ~/.netrc
 
 echo '$ echo $USER'
 echo $USER
-echo '$ ls -al config/'
-ls -al config/
+
 echo '$ ls -al ~ | grep netrc'
 ls -al ~ | grep netrc
